634ca713fe5eed7022c5586763b0f216517192ec
katya pesnya <katyapesnya@icloud.com>
2021-04-28 13:15:52 +0300

    Close Popups

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,43 @@
-
+import React from 'react';
 import '../index.css';
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
 import PopupWithForm from './PopupWithForm'
 import ImagePopup from "./ImagePopup";
+
 function App() {
+
+    const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false);
+    const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
+    const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
+    function handleEditProfileClick() {
+        setEditProfilePopupOpen(!isEditProfilePopupOpen);
+    }
+
+    function handleAddPlaceClick() {
+        setAddPlacePopupOpen(!isAddPlacePopupOpen);
+    }
+
+    function handleEditAvatarClick() {
+        setEditAvatarPopupOpen(!isEditAvatarPopupOpen);
+    }
+    function closeAllPopups(){
+        setEditProfilePopupOpen(false)
+        setAddPlacePopupOpen(false)
+        setEditAvatarPopupOpen(false)
+
+    }
   return (
     <div className="App">
 < div className="root">
       <div className="page">
         <Header />
-        <Main />
+        <Main
+            onEditProfile={handleEditProfileClick}
+            onAddPlace={handleAddPlaceClick}
+            onEditAvatar={handleEditAvatarClick}
+        />
         <Footer />
       </div>
     <PopupWithForm
@@ -21,28 +47,32 @@ function App() {
         children={
             <>
                 <input id="name-input" type="text" name="title" minLength='2' maxLength='40' required
-                       className="popup__input"></input>
-                <span className="popup__input-error name-input-error"></span>
+                       className="popup__input" />
+                <span className="popup__input-error name-input-error" />
                 <input id="job-input" type="text" name="description" minLength='2' maxLength='200' required
-                       className="popup__input"></input>
-                <span className="popup__input-error job-input-error"></span>
+                       className="popup__input" />
+                <span className="popup__input-error job-input-error" />
             </>
         }
         submitMessage="Сохранить"
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
             />
     <PopupWithForm
         name="add"
         title="Новое место"
         children={
             <>
-            <input id="title-input" type="text" name="caption" required minLength='2' maxLength='30' placeholder="Название" className="popup__input"></input>
-                <span className="popup__input-error title-input-error"></span>
-                <input id="url-input" type="url" name="url" required placeholder="Ссылка на картинку" className="popup__input"></input>
-                <span className="popup__input-error url-input-error"></span>
+            <input id="title-input" type="text" name="caption" required minLength='2' maxLength='30' placeholder="Название" className="popup__input"/>
+                <span className="popup__input-error title-input-error" />
+                <input id="url-input" type="url" name="url" required placeholder="Ссылка на картинку" className="popup__input"/>
+                <span className="popup__input-error url-input-error" />
 
             </>
         }
         submitMessage="Создать"
+        isOpen={isAddPlacePopupOpen}
+        onClose={closeAllPopups}
     />
     <PopupWithForm
         name="delete-card"
@@ -52,28 +82,31 @@ function App() {
             </>
         }
         submitMessage="Да"
+
     />
     <PopupWithForm
         name="avatar"
         title="Обновить аватар"
         children={
             <>
-                <input id="avatar-input" type="url" name="avatar"  required className="popup__input"></input>
-                <span className="popup__input-error avatar-input-error"></span>
+                <input id="avatar-input" type="url" name="avatar"  required className="popup__input" />
+                <span className="popup__input-error avatar-input-error" />
             </>
         }
         submitMessage="Сохранить"
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}
     />
     <ImagePopup />
 
   <template className="card-template">
     <li className="card">
-        <img className="card__image" src ='#' alt='#'></img>
-        <button type="button" className="card__delete"></button>
+        <img className="card__image" src ='#' alt='#' />
+        <button type="button" className="card__delete"/>
         <div className="card__item">
-          <h2 className="card__title text-content"></h2>
+          <h2 className="card__title text-content" > </h2>
           <div className="card__like-container">
-            <button type="button" className="card__like"></button>
+            <button type="button" className="card__like"/>
             <span className="card__like-count">0</span>
           </div>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import profileAvatar from '../images/profile-avatar.jpg'
 
-function Main() {
+
+function Main(props) {
     return (
         <main className="content">
             <section className="profile page__content">
                 <div className="profile__overlay">
                     <img src= {profileAvatar} alt="#" className="profile__avatar" />
-                    <button  onClick={handleEditAvatarClick}
-                             className="profile__avatar-edit-button" ></button>
+                    <button  onClick={props.onEditAvatar}
+                             className="profile__avatar-edit-button" />
                 </div>
                 <div className="profile__info">
                     <h1 className="profile__title text-content">Жак-Ив Кусто</h1>
                     <p className="profile__description text-content">Исследователь океана</p>
-                    <button onClick={handleEditProfileClick}
+                    <button onClick={props.onEditProfile}
                             type="button"
-                            className="profile__popup-button"></button>
+                            className="profile__popup-button" />
                 </div>
-                <button onClick={handleAddPlaceClick}
+                <button onClick={props.onAddPlace}
                         type="button"
-                        className="profile__add-button"></button>
+                        className="profile__add-button" />
             </section>
             <section className="cards" aria-label="#">
                 <ul className="cards__list">
@@ -30,16 +31,3 @@ function Main() {
 }
 export default Main;
 
-function handleEditAvatarClick(){
-   document.querySelector('.popup_type_avatar').parentElement.classList.toggle('overlay_opened')
-
-}
-function   handleEditProfileClick(){
-        document.querySelector('.popup_type_edit').parentElement.classList.toggle('overlay_opened')
-
-}
-
-function handleAddPlaceClick (){
-    document.querySelector('.popup_type_add').parentElement.classList.toggle('overlay_opened')
-
-}
