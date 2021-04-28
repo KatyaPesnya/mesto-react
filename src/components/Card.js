a364879934dc39c6f0f86a0e5e3e03caeca1aa89
katya pesnya <katyapesnya@icloud.com>
2021-04-28 22:30:49 +0300

    Show full size image on click

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function Card(props) {
-    function handleClick() {
+    function handleCardClick() {
         props.onCardClick(props.card)
     }
     return(
 
         <li className="card">
-            <img className="card__image" onClick={handleClick} src={props.card.link} alt={`Фотография ${props.card.name}`} />
+            <img className="card__image" onClick={handleCardClick} src={props.card.link} alt={`Фотография ${props.card.name}`} />
             <button type="button" className="card__delete"/>
             <div className="card__item">
                 <h2 className="card__title text-content" > </h2>
